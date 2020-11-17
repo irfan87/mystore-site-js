6cf27da2f0b6e177f7d4b0e0c26c887e46b2baf9
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-17 23:05:58 +0800

    reconfigure .eslintrc for indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,11 @@ module.exports = {
 		ecmaVersion: 11,
 	},
 	rules: {
-		indent: [2, 'tab'],
 		'no-tabs': 0,
 		'no-console': 0,
 		'linebreak-style': 0,
 		quotes: ['error', 'single'],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
 		'no-underscore-dangle': 0,
 		'no-nested-ternary': 0,
 		'import/prefer-default-export': 0,

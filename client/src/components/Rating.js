4ae7e40246a7fde01ceab277c3490df169ffff36
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-18 11:28:37 +0800

    fixed rating value

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,6 +1,6 @@
 const Rating = {
-	render: (props) => {
-		if (!props.value) {
+	render: ({ value, text }) => {
+		if (!value) {
 			return '<div></div>';
 		}
 
@@ -8,50 +8,50 @@ const Rating = {
       <div class="rating">
         <span>
           <i class="${
-						props.value >= 1
+						value >= 1
 							? 'fa fa-star'
-							: props.value > 0.5
+							: value >= 0.5
 							? 'fa fa-star-half-o'
 							: 'fa fa-star-o'
 					}"></i>
         </span>
         <span>
           <i class="${
-						props.value >= 2
+						value >= 2
 							? 'fa fa-star'
-							: props.value > 1.5
+							: value >= 1.5
 							? 'fa fa-star-half-o'
 							: 'fa fa-star-o'
 					}"></i>
         </span>
         <span>
           <i class="${
-						props.value >= 3
+						value >= 3
 							? 'fa fa-star'
-							: props.value > 2.5
+							: value >= 2.5
 							? 'fa fa-star-half-o'
 							: 'fa fa-star-o'
 					}"></i>
         </span>
         <span>
           <i class="${
-						props.value >= 4
+						value >= 4
 							? 'fa fa-star'
-							: props.value > 3.5
+							: value >= 3.5
 							? 'fa fa-star-half-o'
 							: 'fa fa-star-o'
 					}"></i>
         </span>
         <span>
           <i class="${
-						props.value >= 5
+						value >= 5
 							? 'fa fa-star'
-							: props.value > 4.5
+							: value >= 4.5
 							? 'fa fa-star-half-o'
 							: 'fa fa-star-o'
 					}"></i>
         </span>
-        <span>${props.text || ''}</span>
+        <span>${text || ''}</span>
       </div>
     `;
 	},

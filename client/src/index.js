84a2a8b40e10ca1defb32871e6b73b7ecc1d4598
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-18 13:56:41 +0800

    design the product details page

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,26 @@
-import HomeScreen from "./screens/HomeScreen.js";
-import ProductScreen from "./screens/ProductScreen.js";
-import Error404Screen from "./screens/Error404Screen.js";
-import { parseRequestUrl } from "./utils.js";
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Error404Screen from './screens/Error404Screen';
+import { parseRequestUrl } from './utils';
 
 const routes = {
-	"/": HomeScreen,
-	"/product/:id": ProductScreen,
+	'/': HomeScreen,
+	'/product/:id': ProductScreen,
 };
 
 const router = async () => {
 	const request = parseRequestUrl();
 	const parseUrl =
-		(request.resource ? `/${request.resource}` : "/") +
-		(request.id ? "/:id" : "") +
-		(request.verb ? `/${request.verb}` : "");
+		(request.resource ? `/${request.resource}` : '/') +
+		(request.id ? '/:id' : '') +
+		(request.verb ? `/${request.verb}` : '');
 
 	const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
 
-	const main = document.querySelector("#main__container");
+	const main = document.querySelector('#main__container');
 
 	main.innerHTML = await screen.render();
 };
 
-window.addEventListener("load", router);
-window.addEventListener("hashchange", router);
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
